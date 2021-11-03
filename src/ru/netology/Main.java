7e9bbc244d1309942ac3d28340924ce1cbd3835b
Michael <dize882@gmail.com>
2021-11-03 22:24:49 +0300

    Домашка 1.4.2 Задача 2. ИПшник

--- src/ru/netology/Main.java
+++ src/ru/netology/Main.java
@@ -5,54 +5,59 @@ import java.util.*;
 class Main {
 
     final static String STR_PROGRAM_END = "end";
+
     final static String STR_PROGRAM_END_MESSAGE = "Программа завершена!";
     final static String STR_PROGRAM_GREETINGS_MESSAGE = "Здравствуйте!";
     final static String STR_SELECT_OPERATION_MESSAGE = "Выберите операцию и введите её номер:";
-    final static String STR_ENTER_EARNINGS = "Введите сумму дохода:";
-    final static String STR_ENTER_SPENDING = "Введите сумму расхода:";
     final static String STR_NO_OPERATION_MESSAGE = "Такой операции нет!";
-    final static String STR_NO_TAX_SYSTEM_MESSAGE = "Такой системы налогооблажения нет!";
-    final static String STR_TAX_SYSTEM_TIP_MESSAGE = "Мы советуем Вам выбрать:";
+    final static String STR_ENTER_EARNINGS_MESSAGE = "Введите сумму дохода:";
+    final static String STR_ENTER_SPENDING_MESSAGE = "Введите сумму расхода:";
+
     final static String STR_EARNING_OPERATION = "Добавить новый доход";
     final static String STR_SPENDING_OPERATION = "Добавить новый расход";
     final static String STR_TAX_TIP_OPERATION = "Выбрать систему налогооблажения";
-    final static String STR_TAX_RESULT = "Ваш налог составит:";
+
     final static String STR_PAYMENT_SYSTEM = "рублей";
-    final static String STR_TAX_OTHER_SYSTEM_RESULT = "Налог на другой системе:";
+
     final static String STR_SAVINGS_RESULT = "Экономия";
+    final static String STR_TAX_OTHER_SYSTEM_RESULT = "Налог на другой системе:";
+    final static String STR_TAX_SYSTEM_TIP_RESULT = "Мы советуем Вам выбрать:";
+    final static String STR_TAX_RESULT_RESULT = "Ваш налог составит:";
+
     final static String STR_TAX_SYSTEM_ONE_RU = "УСН доходы";
     final static String STR_TAX_SYSTEM_TWO_RU = "УСН доходы минус расходы";
+    final static char WHITESPACE_CHAR = '\u00A0';
 
-    static int earnings = 0;
-    static int spending = 0;
-    static int saving = 0;
-    static int taxEarningsOnly = 0;
-    static int taxEarningsMinusSpending = 0;
+    static int earnings = 0; // доходы
+    static int spending = 0; // расходы
+    static int saving = 0; // экономия
+    static int bestTaxValue = 0; // налог по самой выгодной системе
+    static int otherTaxValue = 0; // налог по другой системе
 
     static String bestTaxSystem = "";
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        String input = new String();
         System.out.println(STR_PROGRAM_GREETINGS_MESSAGE);
         while (true) {
             System.out.println(STR_SELECT_OPERATION_MESSAGE);
             System.out.println("1. " + STR_EARNING_OPERATION);
             System.out.println("2. " + STR_SPENDING_OPERATION);
             System.out.println("3. " + STR_TAX_TIP_OPERATION);
-            input = scanner.nextLine();
+            String input = scanner.nextLine();
             if (STR_PROGRAM_END.equals(input)) {
                 break;
             }
-            switch (input) {
-                case "1":
+            int operation = Integer.parseInt(input);
+            switch (operation) {
+                case 1:
                     setEarnings(scanner);
                     break;
-                case "2":
+                case 2:
                     setSpending(scanner);
                     break;
-                case "3":
+                case 3:
                     setBestTaxSystem();
                     setSaving();
                     showResult();
@@ -65,27 +70,21 @@ class Main {
         System.out.println(STR_PROGRAM_END_MESSAGE);
     }
 
-    /**
-     * Мы советуем вам УСН доходы
-     * Ваш налог составит: 60 рублей
-     * Налог на другой системе: 135 рублей
-     * Экономия: 75 рублей
-     */
     public static void showResult() {
-        System.out.println(STR_TAX_SYSTEM_TIP_MESSAGE + '\t' + getBestTaxSystem());
-        System.out.println(STR_TAX_RESULT + '\t' + getTaxEarningsOnly() + '\t' + STR_PAYMENT_SYSTEM);
-        System.out.println(STR_TAX_OTHER_SYSTEM_RESULT + '\t' + '\t' + STR_PAYMENT_SYSTEM);
-        System.out.println(STR_SAVINGS_RESULT + '\t' + getSaving()  + '\t' + STR_PAYMENT_SYSTEM);
+        System.out.println(STR_TAX_SYSTEM_TIP_RESULT + WHITESPACE_CHAR + getBestTaxSystem());
+        System.out.println(STR_TAX_RESULT_RESULT + WHITESPACE_CHAR + getBestTaxValue() + WHITESPACE_CHAR + STR_PAYMENT_SYSTEM);
+        System.out.println(STR_TAX_OTHER_SYSTEM_RESULT + WHITESPACE_CHAR + getOtherTaxValue() + WHITESPACE_CHAR + STR_PAYMENT_SYSTEM);
+        System.out.println(STR_SAVINGS_RESULT + WHITESPACE_CHAR + getSaving() + WHITESPACE_CHAR + STR_PAYMENT_SYSTEM);
     }
 
     public static void setEarnings(Scanner scanner) {
-        System.out.println(STR_ENTER_EARNINGS);
+        System.out.println(STR_ENTER_EARNINGS_MESSAGE);
         final int input = Integer.parseInt((scanner.nextLine()));
         setEarnings(input);
     }
 
     public static void setSpending(Scanner scanner) {
-        System.out.println(STR_ENTER_SPENDING);
+        System.out.println(STR_ENTER_SPENDING_MESSAGE);
         final int input = Integer.parseInt((scanner.nextLine()));
         setSpending(input);
     }
@@ -103,13 +102,14 @@ class Main {
         final int taxEarningsMinusSpending = taxEarningsMinusSpending();
         final int taxEarningsOnly = taxEarningsOnly();
         setBestTaxSystem(taxEarningsOnly < taxEarningsMinusSpending ? STR_TAX_SYSTEM_ONE_RU : STR_TAX_SYSTEM_TWO_RU);
-        setTaxEarningsOnly(Math.min(taxEarningsOnly, taxEarningsMinusSpending));
+        setBestTaxValue(Math.min(taxEarningsOnly, taxEarningsMinusSpending));
+        setOtherTaxValue(Math.max(taxEarningsOnly, taxEarningsMinusSpending));
     }
 
     public static void setSaving() {
         final int taxEarningsMinusSpending = taxEarningsMinusSpending();
         final int taxEarningsOnly = taxEarningsOnly();
-        setSaving(Math.abs(taxEarningsOnly-taxEarningsMinusSpending));
+        setSaving(Math.abs(taxEarningsOnly - taxEarningsMinusSpending));
     }
 
     public static int getEarnings() {
@@ -136,20 +136,20 @@ class Main {
         saving = tax;
     }
 
-    public static int getTaxEarningsOnly() {
-        return taxEarningsOnly;
+    public static int getBestTaxValue() {
+        return bestTaxValue;
     }
 
-    public static void setTaxEarningsOnly(final int taxEarningsOnly) {
-        Main.taxEarningsOnly = taxEarningsOnly;
+    public static void setBestTaxValue(final int bestTaxValue) {
+        Main.bestTaxValue = bestTaxValue;
     }
 
-    public static int getTaxEarningsMinusSpending() {
-        return taxEarningsMinusSpending;
+    public static int getOtherTaxValue() {
+        return otherTaxValue;
     }
 
-    public static void setTaxEarningsMinusSpending(final int tax) {
-        taxEarningsMinusSpending = tax;
+    public static void setOtherTaxValue(final int tax) {
+        otherTaxValue = tax;
     }
 
     public static String getBestTaxSystem() {
