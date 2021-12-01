64d200334b6a62637a15ab8ecb49f269089e2db9
Michael <dize882@gmail.com>
2021-12-01 19:24:24 +0300

    ДЗ 3.3.1 Библиотека

--- src/ru/netology/Main.java
+++ src/ru/netology/Main.java
@@ -1,31 +1,9 @@
 package ru.netology;
 
 
-import ru.netology.Bill.Bill;
-import ru.netology.TaxService.TaxService;
-import ru.netology.taxes.IncomeTaxType.IncomeTaxType;
-import ru.netology.taxes.ProgressiveTaxType.ProgressiveTaxType;
-import ru.netology.taxes.VATaxType.VATaxType;
-
-import java.math.BigDecimal;
-
 class Main {
 
     public static void main(String[] args) {
-
-        TaxService taxService = new TaxService();
-
-        Bill[] payments = new Bill[]{
-
-                new Bill(BigDecimal.valueOf(510_000_000), new IncomeTaxType(), taxService),
-                new Bill(BigDecimal.valueOf(10_000_000), new ProgressiveTaxType(), taxService),
-                new Bill(BigDecimal.valueOf(10_000), new VATaxType(), taxService),
-                new Bill(BigDecimal.valueOf(2101), new ProgressiveTaxType(), taxService)};
-
-        for (Bill bill : payments) {
-
-            bill.payTaxes();
-        }
 
     }
 }
